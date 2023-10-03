ecf9c86f7126119f4110899fab6f2f71b8d0a61b
Michael <michal.owsiejew23@gmail.com>
2023-10-03 20:54:09 +0100

    Commit

---
+++ app/page.tsx
@@ -0,0 +1,44 @@
+import HeroCarousel from "@/components/HeroCarousel"
+import ProductCard from "@/components/ProductCard";
+import Searchbar from "@/components/Searchbar"
+import { getAllProducts } from "@/lib/actions"
+
+const Home = async () => {
+  const allProducts = await getAllProducts();
+
+  return (
+    <>
+      <section className="px-6 md:px-20 py-24">
+        <div className="flex max-xl:flex-col gap-16">
+          <div className="flex flex-col justify-center">
+
+            <h1 className="head-text">
+              Unleash the power of
+              <span className="text-primary"> Pricelytics</span>
+            </h1>
+
+            <p className="mt-6">
+              Powerful, self-serve product research and competitor analysis to help you crush the competition.
+            </p>
+
+            <Searchbar />
+          </div>
+
+          <HeroCarousel />
+        </div>
+      </section>
+
+      <section className="trending-section">
+        <h2 className="section-text">Trending</h2>
+
+        <div className="flex flex-wrap gap-x-8 gap-y-16">
+          {allProducts?.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Home
